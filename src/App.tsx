831b7e426805d79a715c5378a8853f9fc002b22b
KrissKry <kryczka.krzysztof7@gmail.com>
2023-05-12 15:27:08 +0200

    added test form

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "./common/index.common.css";
+
+import { useForm } from "react-hook-form";
+
+import Button from "./components/Button";
+import Form from "./components/Form";
+import FormInput from "./components/FormInput";
+import FormSelect from "./components/FormSelect";
+import { mealInputs, mealOptions } from "./models";
+import { IDish, DishType, SoupType } from "./types/meals";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const onSubmit = (e: unknown) => {
+        console.log(e as SoupType);
+    };
+
+    const methods = useForm();
+    const type: DishType = methods.watch("type");
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <div className="form-wrapper">
+            <Form<IDish> onSuccess={onSubmit} className="form" methods={methods}>
+                <FormInput inputName="name" type="string" placeholder="nazwa dania" />
+
+                <FormSelect inputName="type" displayName="Rodzaj dania" values={mealOptions} />
+
+                <div style={{ display: type !== "pizza" ? "none" : "inherit" }}>
+                    {mealInputs.pizzaInputs.map((i) => (
+                        <FormInput key={i.inputName} {...i} disabled={type !== "pizza"} />
+                    ))}
+                </div>
+
+                <div style={{ display: type !== "soup" ? "none" : "inherit" }}>
+                    {mealInputs.soupInputs.map((i) => (
+                        <FormInput key={i.inputName} {...i} disabled={type !== "soup"} />
+                    ))}
+                </div>
+
+                <div style={{ display: type !== "sandwich" ? "none" : "inherit" }}>
+                    {mealInputs.sandwichInputs.map((i) => (
+                        <FormInput key={i.inputName} {...i} disabled={type !== "sandwich"} />
+                    ))}
+                </div>
+
+                <Button
+                    type="submit"
+                    text="submit"
+                    uppercase
+                    thin
+                    primary
+                    hover
+                    className="margin-v1"
+                />
+            </Form>
+        </div>
     );
 }
 
